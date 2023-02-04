f6a2b3e1cdaab929f6a6ce736bb2024b042cfca3
pengbo <baoyun20@gmail.com>
2023-02-04 23:59:10 +0800

    finish crop and fusion

--- crop_fuse.py
+++ crop_fuse.py
@@ -9,6 +9,7 @@ Description:
 
 import cv2
 import numpy as np
+import pdb
 
 
 def crop_tetragon(img, src_pts=np.float32([[10, 10], [200, 20],
@@ -23,19 +24,28 @@ def crop_tetragon(img, src_pts=np.float32([[10, 10], [200, 20],
         M, (img_width, img_height),
         #borderMode=cv2.BORDER_REPLICATE,
         flags=cv2.INTER_CUBIC)
-    
     reverse_height, reverse_width = img.shape[:2]
     reverse_img = cv2.warpPerspective(
         dst_img,
         M_reverse, (reverse_width, reverse_height),
         #borderMode=cv2.BORDER_REPLICATE,
         flags=cv2.INTER_CUBIC)
-
+    
     return dst_img, reverse_img
+
+
+def fusion_imgs(img1, img2):
+    mask = (img2[:,:,0]+1)==0
+    _, mask = cv2.threshold(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+    img_bg = cv2.bitwise_and(img1, img1, mask=mask_inv)
+    img_fg = cv2.bitwise_and(img2, img2, mask=mask)
+    fuse_img = cv2.add(img_bg, img_fg)
+    return fuse_img
 
 
 if __name__ == "__main__":
     img = cv2.imread('data/test_vis.jpg')
-    crop_img, reverse_img = crop_tetragon(img)
-    cv2.imwrite('data/crop_test.jpg', crop_img)
-    cv2.imwrite('data/reverse_test.jpg', reverse_img)
+    _, reverse_img = crop_tetragon(img)
+    fuse_img = fusion_imgs(img, reverse_img)
+    cv2.imwrite('data/fuse_img.jpg', fuse_img)
