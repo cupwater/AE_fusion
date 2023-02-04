7a66fbccd6e249772eefef4a792bdc1b24c3fa3b
pengbo <baoyun20@gmail.com>
2023-02-04 16:56:04 +0800

    add reverse

--- crop_tetragon.py
+++ crop_tetragon.py
@@ -16,16 +16,26 @@ def crop_tetragon(img, src_pts=np.float32([[10, 10], [200, 20],
                           dst_pts=np.float32([[0, 0], [512, 0],
                                         [0, 640], [512, 640]])):
     M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    M_reverse = cv2.getPerspectiveTransform(dst_pts, src_pts)
     img_width, img_height = 512, 640
     dst_img = cv2.warpPerspective(
         img,
         M, (img_width, img_height),
-        borderMode=cv2.BORDER_REPLICATE,
+        #borderMode=cv2.BORDER_REPLICATE,
         flags=cv2.INTER_CUBIC)
-    return dst_img
+    
+    reverse_height, reverse_width = img.shape[:2]
+    reverse_img = cv2.warpPerspective(
+        dst_img,
+        M_reverse, (reverse_width, reverse_height),
+        #borderMode=cv2.BORDER_REPLICATE,
+        flags=cv2.INTER_CUBIC)
+
+    return dst_img, reverse_img
 
 
 if __name__ == "__main__":
-    img = cv2.imread('data/1.jpg')
-    crop_img = crop_tetragon(img)
+    img = cv2.imread('data/test_vis.jpg')
+    crop_img, reverse_img = crop_tetragon(img)
     cv2.imwrite('data/crop_test.jpg', crop_img)
+    cv2.imwrite('data/reverse_test.jpg', reverse_img)
