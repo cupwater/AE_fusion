82317a3ae87e80330d07f24d769a2b957f2b2f5d
pengbo <baoyun20@gmail.com>
2023-02-20 21:56:40 +0800

    add dehaze video testing code

---
+++ test_dehaze.py
@@ -0,0 +1,70 @@
+'''
+Author: Peng Bo
+Date: 2022-11-13 22:19:55
+LastEditTime: 2023-02-09 11:19:40
+Description: 
+
+'''
+import time
+import cv2
+import numpy as np
+import torch
+import onnxruntime as ort
+from models.aod_net import AODnet
+
+# pre-process function for dehaze model
+def preprocess(ori_image, input_size):
+    image = cv2.resize(ori_image, input_size)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.astype(np.float32)/255.0
+    image = (image - np.array([0.5, 0.5, 0.5]))
+    image = np.divide(image, np.array([0.5, 0.5, 0.5]))
+    image = np.expand_dims(np.transpose(image, [2, 0, 1]), axis=0)
+    image = image.astype(np.float32)
+    return image
+
+
+# inference using pytorch model
+def dehaze_pt(image, pt_model, input_size=(1920, 1080)):  
+    processed_frame  = preprocess(image, input_size)
+    # inference using pytorch model 
+    pt_output = pt_model(torch.FloatTensor(torch.from_numpy(processed_frame)))
+    # convert the output to image
+    pt_output = pt_output.data.cpu().numpy().squeeze().transpose((1,2,0))*255
+    dehaze_image = pt_output.astype(np.uint8)[:,:,::-1]
+    return dehaze_image
+
+
+# inference using onnx model
+def dehaze_onnx(image, ort_session, input_size=(1920, 1080)):  
+    input_name = ort_session.get_inputs()[0].name
+    input  = preprocess(image, input_size)
+    onnx_output = ort_session.run(None, {input_name: input})[0][0]
+    onnx_putput = onnx_output.transpose((1,2,0))*255
+    dehaze_image = onnx_putput.astype(np.uint8)[:,:,::-1]
+    return dehaze_image
+
+
+if __name__ == '__main__':
+
+    pt_model = AODnet()
+    pt_model.load_state_dict(torch.load("dehaze_data/AODNet_dehaze.pth", map_location='cpu'))
+    pt_model.eval()
+
+    onnx_model = ort.InferenceSession("dehaze_data/AODNet_Dehaze.onnx")
+    cap = cv2.VideoCapture('data/haze-videos/fog.mp4')
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (1920, 1080))
+        if not ret:
+            break
+        dehaze_image_pt   = dehaze_pt(frame, pt_model)
+        dehaze_image_onnx = dehaze_onnx(frame, onnx_model)
+
+        cv2.imshow("haze image", frame)
+        cv2.imshow("dehaze image using pt_model", dehaze_image_pt)
+        cv2.imshow("dehaze image using onnx model", dehaze_image_onnx)
+
+        key = cv2.waitKey(1)
+        if key==27 or key == ord("q"):
+            exit(0)
---
+++ test_fusion.py
@@ -0,0 +1,50 @@
+'''
+Author: Peng Bo
+Date: 2022-11-13 22:19:55
+LastEditTime: 2023-02-09 11:19:40
+Description: 
+
+'''
+import time
+import cv2
+import numpy as np
+import onnxruntime as ort
+from crop_fuse import fusion_imgs, crop_tetragon
+
+def fuse_vis_ir(vis_image, ir_image, ort_session, input_size=(512, 640)):    
+    input_name_1 = ort_session.get_inputs()[0].name
+    input_name_2 = ort_session.get_inputs()[1].name
+    def _preprocess(src_image):
+        src_image = cv2.resize(src_image, input_size)
+        # pre-process the input image 
+        if len(src_image.shape) == 3:
+            src_image = cv2.cvtColor(src_image, cv2.COLOR_BGR2GRAY)
+        input_data = np.expand_dims(src_image, axis=2)
+        input_data = (input_data/255.0)
+        target_data = np.expand_dims(np.transpose(input_data, [2, 0, 1]), axis=0)
+        target_data = target_data.astype(np.float32)
+        return target_data
+
+    vis_input = _preprocess(vis_image)
+    ir_input  = _preprocess(ir_image)
+    fuse_image = ort_session.run(None, {input_name_1: vis_input, input_name_2: ir_input})
+    return fuse_image
+
+if __name__ == '__main__':
+    onnx_path = "experiments/template/ae_fusion.onnx"
+    ort_session = ort.InferenceSession(onnx_path)
+    vis_image = cv2.imread("data/test_vis.jpg")
+    ir_image  = cv2.imread("data/test_ir.jpg")
+
+    for i in range(20):
+        fuse_image = fuse_vis_ir(vis_image, ir_image, ort_session)[0][0][0]
+        start_time = time.time()
+        _, reverse_img = crop_tetragon(vis_image)
+        fuse_img = fusion_imgs(vis_image, reverse_img)
+        print("inference time:{}".format(time.time() - start_time))
+
+    from skimage.io import imsave
+    imsave(f"data/fuse.jpg", fuse_image)
+
+    import onnx_tool
+    onnx_tool.model_profile(onnx_path, None, None) # pass file name
