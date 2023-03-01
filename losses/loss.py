fix adap gradient loss bug
fix gradient bug

fix gradient loss bug

fix bug

losses/loss.py
@@ -2,7 +2,7 @@
 '''
 Author: Pengbo
 Date: 2022-02-23 16:17:31
-LastEditTime: 2023-03-01 18:29:42
+LastEditTime: 2023-03-01 21:21:33
 Description: loss function
 
 '''
@@ -11,25 +11,29 @@ from torch import nn
 from torch.nn import functional as F
 import kornia
 
-from utils.utils import low_pass
+from utils.utils import low_pass, gradient
 
 
-__all__ = ['L1Loss', 'L2Loss', 'MS_SSIMLoss', 'GradientL1Loss', 'GradientL2Loss', 'AdaptiveGradientL2Loss', 'BCELoss', 'BCEFocalLoss', 'FocalLoss']
+__all__ = ['L1Loss', 'L2Loss', 'MS_SSIMLoss', 'GradientL1Loss', 'GradientL2Loss',
+           'AdaptiveGradientL2Loss', 'BCELoss', 'BCEFocalLoss', 'FocalLoss']
+
 
 class L1Loss(nn.Module):
-    def __init__(self):
+    def __init__(self, reduction='mean'):
         super(L1Loss, self).__init__()
+        self.reduction = reduction
 
     def forward(self, input, target):
-        return F.l1_loss(input, target, reduction='mean')
+        return F.l1_loss(input, target, reduction=self.reduction)
 
 
 class L2Loss(nn.Module):
-    def __init__(self):
+    def __init__(self, reduction='mean'):
         super(L2Loss, self).__init__()
+        self.reduction = reduction
 
     def forward(self, input, target):
-        return F.mse_loss(input, target, reduction='mean')
+        return F.mse_loss(input, target, reduction=self.reduction)
 
 
 class BCELoss(nn.Module):
@@ -41,15 +45,16 @@ class BCELoss(nn.Module):
     def forward(self, logits, target):
         # logits: [N, *], target: [N, *]
         loss = - self.pos_weight * target * torch.log(logits) - \
-               (1 - target) * torch.log(1 - logits)
+            (1 - target) * torch.log(1 - logits)
         if self.reduction == 'mean':
             loss = loss.mean()
         elif self.reduction == 'sum':
             loss = loss.sum()
         return loss
 
+
 class BCEFocalLoss(nn.Module):
-    def __init__(self, alpha=0.6, gamma=0.2, num_classes = 2):
+    def __init__(self, alpha=0.6, gamma=0.2, num_classes=2):
         super(BCEFocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
@@ -60,11 +65,13 @@ class BCEFocalLoss(nn.Module):
         alpha = self.alpha
         gamma = self.gamma
         loss = - alpha * (1 - logits) ** gamma * target * torch.log(logits) - \
-               (1 - alpha) * logits ** gamma * (1 - target) * torch.log(1 - logits)
+            (1 - alpha) * logits ** gamma * \
+            (1 - target) * torch.log(1 - logits)
         return loss.mean()
 
+
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=0.25, gamma=2, num_classes = 2, size_average=True):
+    def __init__(self, alpha=0.25, gamma=2, num_classes=2, size_average=True):
         """
         focal_loss, -α(1-yi)**γ *ce_loss(xi,yi)
         :param alpha: loss weight for each class. 
@@ -72,16 +79,17 @@ class FocalLoss(nn.Module):
         :param num_classes:
         :param size_average:
         """
-        super(FocalLoss,self).__init__()
+        super(FocalLoss, self).__init__()
         self.size_average = size_average
-        if isinstance(alpha,list):
-            assert len(alpha)==num_classes
+        if isinstance(alpha, list):
+            assert len(alpha) == num_classes
             self.alpha = torch.Tensor(alpha)
         else:
-            assert alpha<1 
+            assert alpha < 1
             self.alpha = torch.zeros(num_classes)
             self.alpha[0] += alpha
-            self.alpha[1:] += (1-alpha) # set alpha to [ α, 1-α, 1-α, 1-α, 1-α, ...] size:[num_classes]
+            # set alpha to [ α, 1-α, 1-α, 1-α, 1-α, ...] size:[num_classes]
+            self.alpha[1:] += (1-alpha)
         self.gamma = gamma
 
     def forward(self, preds, labels):
@@ -91,15 +99,16 @@ class FocalLoss(nn.Module):
         :return:
         """
         # assert preds.dim()==2 and labels.dim()==1
-        preds = preds.view(-1,preds.size(-1))
+        preds = preds.view(-1, preds.size(-1))
         self.alpha = self.alpha.to(preds.device)
-        preds_logsoft = F.log_softmax(preds, dim=1) # log_softmax
+        preds_logsoft = F.log_softmax(preds, dim=1)  # log_softmax
         preds_softmax = torch.exp(preds_logsoft)    # softmax
 
-        preds_softmax = preds_softmax.gather(1,labels.view(-1,1))  
-        preds_logsoft = preds_logsoft.gather(1,labels.view(-1,1))
-        self.alpha = self.alpha.gather(0,labels.view(-1))
-        loss = -torch.mul(torch.pow((1-preds_softmax), self.gamma), preds_logsoft) 
+        preds_softmax = preds_softmax.gather(1, labels.view(-1, 1))
+        preds_logsoft = preds_logsoft.gather(1, labels.view(-1, 1))
+        self.alpha = self.alpha.gather(0, labels.view(-1))
+        loss = -torch.mul(torch.pow((1-preds_softmax),
+                          self.gamma), preds_logsoft)
 
         loss = torch.mul(self.alpha, loss.t())
         if self.size_average:
@@ -112,12 +121,14 @@ class FocalLoss(nn.Module):
 class MS_SSIMLoss(nn.Module):
     def __init__(self, window_size=11, max_val=1.0):
         super(MS_SSIMLoss, self).__init__()
-        self.ssim = kornia.losses.SSIMLoss(window_size=window_size, max_val=max_val, reduction='mean')
+        self.ssim = kornia.losses.SSIMLoss(
+            window_size=window_size, max_val=max_val, reduction='mean')
         self.mse = nn.MSELoss()
 
     def forward(self, img1, img2):
         return self.ssim(img1, img2) + self.mse(img1, img2)
-    
+
+
 class GradientL1Loss(nn.Module):
     def __init__(self):
         super(GradientL1Loss, self).__init__()
@@ -125,7 +136,7 @@ class GradientL1Loss(nn.Module):
         self.l1loss = L1Loss()
 
     def forward(self, img1, img2):
-        return self.l1loss( self.gradient_fun(img1), self.gradient_fun(img2) )
+        return self.l1loss(self.gradient_fun(img1), self.gradient_fun(img2))
 
 
 class GradientL2Loss(nn.Module):
@@ -135,20 +146,26 @@ class GradientL2Loss(nn.Module):
         self.l2loss = L2Loss()
 
     def forward(self, img1, img2):
-        return self.l2loss( self.gradient_fun(img1), self.gradient_fun(img2) )
-
+        return self.l2loss(self.gradient_fun(img1), self.gradient_fun(img2))
 
 
 class AdaptiveGradientL2Loss(nn.Module):
     def __init__(self):
         super(AdaptiveGradientL2Loss, self).__init__()
-        self.gradient_fun = kornia.filters.SpatialGradient()
-        self.l2loss = L2Loss()
+        self.l2loss = L2Loss(reduction='')
 
-    def forward(self, img1, img2):
-
-        img1_grad_lowpass = self.gradient_fun(low_pass(img1))
-        img2_grad_lowpass = self.gradient_fun(low_pass(img2))
-        img1_score = torch.sign(img1_grad_lowpass - torch.minimum(img1_grad_lowpass, img2_grad_lowpass))
-
-        return img1_score.detach()*self.l2loss( self.gradient_fun(img1), self.gradient_fun(img2) )
+    def forward(self, fused_img, vis, ir):
+        # 维度对不上，应该是 element-wise
+        vis_grad_lowpass = gradient(
+            low_pass(torch.mean(vis, dim=1, keepdim=True)))
+        ir_grad_lowpass = gradient(
+            low_pass(torch.mean(ir, dim=1, keepdim=True)))
+        vis_score = torch.sign(
+            vis_grad_lowpass - torch.minimum(vis_grad_lowpass, ir_grad_lowpass))
+        ir_score = 1 - vis_score
+        fused_img_grad = gradient(torch.mean(fused_img, dim=1, keepdim=True))
+        vis_grad = gradient(torch.mean(vis, dim=1, keepdim=True))
+        ir_grad = gradient(torch.mean(vis, dim=1, keepdim=True))
+        loss_gradient = torch.mul(vis_score.detach(), torch.square(fused_img_grad - vis_grad) \
+                        + torch.mul(ir_score.detach(), torch.square(fused_img_grad - ir_grad)))
+        return torch.mean(loss_gradient)

train_sdnet.py
@@ -127,8 +127,7 @@ def train(trainloader, model, optimizer, use_cuda, epoch, print_interval=100):
 
     end = time.time()
 
-    criterion_GradLoss = GradientL2Loss()
-    gradfun = kornia.filters.SpatialGradient()
+    criterion_AdapGradLoss = AdaptiveGradientL2Loss()
     model.train()
     for batch_idx, (vis_in, ir_in) in enumerate(trainloader):
         # measure data loading time
@@ -143,14 +142,7 @@ def train(trainloader, model, optimizer, use_cuda, epoch, print_interval=100):
         loss_reconstruct = F.mse_loss(vis_out, vis_in, reduction='mean') + \
                     F.mse_loss(ir_out, ir_in, reduction='mean')
 
-        # 维度对不上，应该是 element-wise 
-        vis_grad_lowpass = gradfun(low_pass(torch.mean(vis_in, dim=1, keepdim=True)))
-        ir_grad_lowpass  = gradfun(low_pass(torch.mean(ir_in, dim=1, keepdim=True)))
-        vis_score = torch.sign(vis_grad_lowpass - torch.minimum(vis_grad_lowpass, ir_grad_lowpass))
-        ir_score  = 1 - vis_score
-
-        loss_gradient = vis_score.detach()*criterion_GradLoss(fused_img, vis_in) + \
-                    ir_score.detach()*criterion_GradLoss(fused_img, ir_in)
+        loss_gradient = criterion_AdapGradLoss(fused_img, vis_in, ir_in)
         all_loss = loss_intensity + 80 * loss_gradient + loss_reconstruct
 
         losses_intensity.update(loss_intensity, vis_in.size(0)) 

utils/utils.py
@@ -1,7 +1,7 @@
 '''
 Author: Peng Bo
 Date: 2023-03-01 17:51:23
-LastEditTime: 2023-03-01 18:04:44
+LastEditTime: 2023-03-01 20:55:01
 Description: 
 
 '''
@@ -15,3 +15,7 @@ def low_pass(input):
                     [0.0947,0.1183,0.0947]]).reshape(1,1,3,3)).cuda()
     return F.conv2d(input, weight, padding=1)
 
+def gradient(input):
+    weight = torch.nn.Parameter(torch.FloatTensor(
+                [[0.,1.,0.],[1.,-4.,1.],[0.,1.,0.]])).reshape(1,1,3,3).cuda()
+    return F.conv2d(input, weight, padding=1) 