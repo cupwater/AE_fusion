719766c87271c96943b4a54fa22b441ca83f96a5
pengbo <baoyun20@gmail.com>
2023-04-05 14:09:18 +0800

    fix crop_fuse bug

--- crop_fuse.py
+++ crop_fuse.py
@@ -11,17 +11,38 @@ import cv2
 import numpy as np
 import pdb
 
+ir_click_list = []
+vis_click_list = []
 
-def crop_tetragon(img, src_pts=np.float32([[10, 10], [200, 20],
+dst_pts = np.float32(
+        [[0, 0],
+        [640, 0],
+        [0, 512],
+        [640, 512]]
+    )
+
+src_pts = np.float32(
+   [[368, 137],
+   [1583, 137],
+   [368, 1075],
+   [1583, 1075]]
+)
+
+
+
+def get_affine_M(src_pts=np.float32([[10, 10], [200, 20],
                                         [30, 250], [250, 250]]) , 
                           dst_pts=np.float32([[0, 0], [512, 0],
                                         [0, 640], [512, 640]])):
     M = cv2.getPerspectiveTransform(src_pts, dst_pts)
     M_reverse = cv2.getPerspectiveTransform(dst_pts, src_pts)
-    img_width, img_height = 512, 640
+    return np.round(M, 3), np.round(M_reverse, 3)
+
+
+def crop_tetragon(img, tgt_size, M, M_reverse):
+    # img_width, img_height = 512, 640
     dst_img = cv2.warpPerspective(
-        img,
-        M, (img_width, img_height),
+        img, M, tgt_size,
         #borderMode=cv2.BORDER_REPLICATE,
         flags=cv2.INTER_CUBIC)
     reverse_height, reverse_width = img.shape[:2]
@@ -34,6 +55,25 @@ def crop_tetragon(img, src_pts=np.float32([[10, 10], [200, 20],
     return dst_img, reverse_img
 
 
+def on_IR_EVENT_BUTTON(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xy = "%d,%d" % (x, y)
+        # cv2.circle(img, (x,y), 1, (0,0,255), thinkness=-1)
+        # cv2.putText(img, xy, (x,y), cv2.FONT_HERSHEY_PLAIN, 1.0, (0,0,0), thickness=1)
+        # cv2.imshow("ir", img)
+        print(x, y)
+        ir_click_list.append([x, y])
+
+
+def on_VIS_EVENT_BUTTON(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xy = "%d,%d" % (x, y)
+        # cv2.circle(img, (x,y), 1, (0,0,255), thinkness=-1)
+        # cv2.putText(img, xy, (x,y), cv2.FONT_HERSHEY_PLAIN, 1.0, (0,0,0), thickness=1)
+        # cv2.imshow("vis", img)
+        vis_click_list.append([x, y])
+        print(x, y)
+
 def fusion_imgs(img1, img2):
     mask = (img2[:,:,0]+1)==0
     _, mask = cv2.threshold(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
@@ -45,7 +85,58 @@ def fusion_imgs(img1, img2):
 
 
 if __name__ == "__main__":
-    img = cv2.imread('fusion_data/test_vis.jpg')
-    _, reverse_img = crop_tetragon(img)
-    fuse_img = fusion_imgs(img, reverse_img)
-    cv2.imwrite('fusion_data/fuse_img.jpg', fuse_img)
+    
+    cv2.namedWindow("vis")
+    cv2.namedWindow("ir")
+    cv2.setMouseCallback("vis", on_VIS_EVENT_BUTTON)
+    cv2.setMouseCallback("ir", on_IR_EVENT_BUTTON)
+
+    # M = np.float32([
+    #         [ 4.92417784e-01, -1.25833390e-01, -1.57544109e+02],
+    #         [-3.73122860e-03, 4.65769967e-01,  -4.10213890e+01],
+    #         [ 7.10971686e-06, -2.37132253e-04, 1.00000000e+00]
+    #     ])
+    # M_reverse =np.float32([
+    #         [1.99246198e+00,  7.12991077e-01, 3.43148532e+02],
+    #         [ 1.50276106e-02, 2.15628043e+00, 9.08211297e+01],
+    #         [-1.06023094e-05, 5.06254471e-04, 1.00000000e+00]
+    #     ])
+
+    M = np.float32(
+       [[0.527,    0.   , -193.844],
+       [   0.   ,    0.546,  -74.78 ],
+       [   0.   ,   -0.   ,    1.   ]]
+    )
+    M_reverse = np.float32(
+       [[  1.8984375 ,   0.        , 368.        ],
+       [  0.        ,   1.83203125, 137.        ],
+       [ -0.        ,  -0.        ,   1.        ]]
+    )
+
+    for i in range(6):
+        ir_click_list = []
+        vis_click_list = []
+
+        ir = cv2.imread(f"fusion_data/fusion_pairs/hongwai_{i}.jpg")
+        vis = cv2.imread(f"fusion_data/fusion_pairs/kejianguang_{i}.jpg")
+        # cv2.imshow("ir", ir)
+        # cv2.imshow("vis", vis)
+        # cv2.waitKey(-1)
+        # src_pts = np.float32(vis_click_list)
+        # dst_pts = np.float32(ir_click_list)
+        tgt_h, tgt_w = ir.shape[:2]
+        # M, M_reverse = get_affine_M(src_pts=src_pts, dst_pts=dst_pts)
+        _, reverse_img = crop_tetragon(vis, (tgt_w, tgt_h), M, M_reverse)
+        fuse_img = fusion_imgs(vis, reverse_img)
+        cv2.imshow("fuse", fuse_img)
+        cv2.waitKey(-1)
+
+        print(M)
+        print(M_reverse)
+        # pdb.set_trace()
+        # cv2.imwrite('fusion_data/fuse_img.jpg', fuse_img)
+
+    # img = cv2.imread('fusion_data/test_vis.jpg')
+    # _, reverse_img = crop_tetragon(img)
+    # fuse_img = fusion_imgs(img, reverse_img)
+    # cv2.imwrite('fusion_data/fuse_img.jpg', fuse_img)
