82ba9e133bffa531dc8303c5765eafd22c9ab45b
pengbo <baoyun20@gmail.com>
2023-07-07 19:31:59 +0800

    re-organize code

--- models/__init__.py
+++ models/__init__.py
@@ -7,8 +7,8 @@ Description:
 '''
 # -*- coding: utf8 -*-
 
-from .ae_fusion import AutoEncoder
-from .SDNet import SDNet, LightSDNet, LightSDNetONNX
-#from .DMPHN_dehaze import DMPHN_Dehaze
-from .aod_net import AODnet, LightAODnet, TinyAODnet, XLTinyAODnet
+from fusion.ae_fusion import AutoEncoder
+from fusion.SDNet import SDNet, LightSDNet, LightSDNetONNX
 from fusion.coa_model import Restormer_Encoder, Restormer_Decoder
+#from .DMPHN_dehaze import DMPHN_Dehaze
+from dehaze.aod_net import AODnet, LightAODnet, TinyAODnet, XLTinyAODnet
